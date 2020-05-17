cbd920fd90c16c51439d479b10f6efea35a53884
iFieryGod <61666517+iFieryGod@users.noreply.github.com>
2020-05-17 19:26:31 +0200

    solved upload error with heroku

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 // Setup to allow us to use HEROKU in production and our own local port locally. in this case port 3000
 if(process.env.NODE_ENV === 'production'){
   module.exports = {
-    mongoURI: 'mongodb+srv://Goran:<Skyrimageofdragons@123>@yum-vlcai.mongodb.net/test?retryWrites=true&w=majority'
+    mongoURI: 'mongodb+srv://Goran:foM8NPKwyicEV3ff@yum-vlcai.mongodb.net/test?retryWrites=true&w=majority'
   }
 } else {
   module.exports = {mongoURI: 'mongodb://localhost/Yummy'}
